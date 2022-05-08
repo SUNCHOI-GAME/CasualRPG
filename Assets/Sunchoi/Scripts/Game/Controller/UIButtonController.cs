a5cb1c8e9e1b27f68abc36ab09d134c7cfcd3ffd
hurutomomk <hurutomomk@gmail.com>
2022-05-08 21:58:34 +0900

    HUD表示切り替えを実装。

--- Assets/Sunchoi/Scripts/Game/Controller/UIButtonController.cs
+++ Assets/Sunchoi/Scripts/Game/Controller/UIButtonController.cs
@@ -19,20 +19,25 @@ public class UIButtonController : MonoBehaviour
 
     [Header(" --- Buttons")]
     /// <summary>
-    /// プレイヤーの移動ボタン
+    /// セッティングメニューボタン
     /// </summary>
     [SerializeField]
     private Button settingButton;
     /// <summary>
-    /// プレイヤーの移動ボタン
+    /// 移動モード切り替えボタン
+    /// </summary>
+    [SerializeField]
+    private Button movementModeToggleButton;
+    /// <summary>
+    /// インベントリーボタン
     /// </summary>
     [SerializeField]
     private Button inventoryButton;
     /// <summary>
-    /// プレイヤーの移動ボタン
+    /// HUD表示切り替えボタン
     /// </summary>
     [SerializeField]
-    private Button toggleButton;
+    private Button hUDActivationToggleButton;
     /// <summary>
     /// プレイヤーの移動ボタン
     /// </summary>
@@ -48,11 +53,6 @@ public class UIButtonController : MonoBehaviour
     #region [03. ボタンオブジェクト]
     [Header(" --- Button Objects")]
     /// <summary>
-    /// 移動ボタン表示切り替え用ToggleボタンのGameObject
-    /// </summary>
-    [SerializeField]
-    private GameObject buttonStateToggleButton;
-    /// <summary>
     /// プレイヤー移動ボタンのGameObject
     /// </summary>
     [SerializeField]
@@ -63,12 +63,39 @@ public class UIButtonController : MonoBehaviour
     [SerializeField]
     private GameObject movementButtonObjForCamera;
     #endregion
+    
+    #region [04. ボタンオブジェクトリスト]
+    [Header(" --- Button Object List")]
+    /// <summary>
+    /// HUD表示切り替え対象のGameObjectリスト
+    /// </summary>
+    [SerializeField]
+    private GameObject[] buttonObjsForHUDActivation;
+    #endregion
+    
+    #region [05. Image]
+    [Header(" --- Button Objects")]
+    /// <summary>
+    /// 移動モード切り替えボタンのImage
+    /// </summary>
+    [SerializeField]
+    private Image movementModeToggleButtonImage;
+    /// <summary>
+    /// HUD表示切り替えボタンのImage
+    /// </summary>
+    [SerializeField]
+    private Image hUDActivationToggleButtonImage;
+    #endregion
 
     #region [04. トリガー]
     /// <summary>
     /// 移動ボタン表示の切り替えトリガー
     /// </summary>
     private bool isButtonForCameraMovement = false;
+    /// <summary>
+    /// HUD表示の切り替えトリガー
+    /// </summary>
+    private bool isHUDActivationOff = false;
     #endregion
 
     #endregion
@@ -83,24 +110,37 @@ public class UIButtonController : MonoBehaviour
     private void Awake()
     {
         // 移動ボタンの初期化
-        this.SetMovementButton(this.isButtonForCameraMovement);
+        this.SetMovementButtonState(this.isButtonForCameraMovement);
     }
     #endregion
     
-    #region [02. 移動ボタンのステート管理]
+    #region [02. ボタンのステート管理]
     /// <summary>
     /// 移動ボタンの表示切り替え
     /// </summary>
     /// <param name="state"></param>
-    public void SetMovementButton(bool state)
+    public void SetMovementButtonState(bool state)
     {
-        // カメラ移動ボタンの表示ステート
+        // カメラ移動ボタンの表示ステート変更
         this.movementButtonObjForCamera.SetActive(state);
-        // プレイヤー移動ボタンの表示ステート
+        // プレイヤー移動ボタンの表示ステート変更
         this.movementButtonObjForPlayer.SetActive(!state);
         
         // カメラ座標のリセット
         this.playerMovementController.ResetCameraPosition();
+    }
+
+    /// <summary>
+    /// HUD表示切り替え
+    /// </summary>
+    /// <param name="state"></param>
+    private void SetHUDActivationState(bool state)
+    {
+        foreach (var button in this.buttonObjsForHUDActivation)
+        {
+            // 対象ボタンの表示ステート変更
+            button.SetActive(state);
+        }
     }
     #endregion
     
@@ -116,7 +156,7 @@ public class UIButtonController : MonoBehaviour
             this.isButtonForCameraMovement = false;
             // ボタンImageの表示切り替え
             // TODO :: アルファ時にSprite切り替えに変更
-            this.buttonStateToggleButton.GetComponent<Image>().color = Color.white;
+            this.movementModeToggleButtonImage.color = Color.white;
         }
         else
         {
@@ -124,11 +164,37 @@ public class UIButtonController : MonoBehaviour
             this.isButtonForCameraMovement = true;
             // ボタンImageの表示切り替え
             // TODO :: アルファ時にSprite切り替えに変更
-            this.buttonStateToggleButton.GetComponent<Image>().color = Color.green;
+            this.movementModeToggleButtonImage.color = Color.green;
         }
         
         // 移動ボタンの表示切り替え
-        this.SetMovementButton(this.isButtonForCameraMovement);
+        this.SetMovementButtonState(this.isButtonForCameraMovement);
+    }
+
+    /// <summary>
+    /// 移動ボタンの表示切り替えボタン押下時の処理
+    /// </summary>
+    public void OnClickChangeHUDActivationStateButton()
+    {
+        if (this.isHUDActivationOff)
+        {
+            // トリガーをセット
+            this.isHUDActivationOff = false;
+            // ボタンImageの表示切り替え
+            // TODO :: アルファ時にSprite切り替えに変更
+            this.hUDActivationToggleButtonImage.color = Color.white;
+        }
+        else
+        {
+            // トリガーをセット
+            this.isHUDActivationOff = true;
+            // ボタンImageの表示切り替え
+            // TODO :: アルファ時にSprite切り替えに変更
+            this.hUDActivationToggleButtonImage.color = Color.green;
+        }
+
+        // HUDの表示切り替え
+        this.SetHUDActivationState(!this.isHUDActivationOff);
     }
     
     /// <summary>
@@ -167,7 +233,8 @@ public class UIButtonController : MonoBehaviour
         // TODO :: ボタンImageの表示切り替え処理を追加
         this.settingButton.enabled = false;
         this.inventoryButton.enabled = false;
-        this.toggleButton.enabled = false;
+        this.movementModeToggleButton.enabled = false;
+        this.movementModeToggleButton.enabled = false;
         foreach (var button in this.movementButtonsForPlayer)　button.enabled = false;
         foreach (var button in this.movementButtonsForCamera)　button.enabled = false;
 
@@ -178,7 +245,8 @@ public class UIButtonController : MonoBehaviour
             // TODO :: ボタンImageの表示切り替え処理を追加
             this.settingButton.enabled = true;
             this.inventoryButton.enabled = true;
-            this.toggleButton.enabled = true;
+            this.movementModeToggleButton.enabled = true;
+            this.movementModeToggleButton.enabled = true;
             foreach (var button in this.movementButtonsForPlayer)　button.enabled = true;
             foreach (var button in this.movementButtonsForCamera)　button.enabled = true;
         });
