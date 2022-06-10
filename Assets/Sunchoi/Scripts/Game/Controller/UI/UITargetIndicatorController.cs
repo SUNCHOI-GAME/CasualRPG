e5feaa80efd7e15825e4f30812288d09ce200925
ChoiJihun <choijihun@sunchoi.co.jp>
2022-06-10 14:55:01 +0900

    カメラのViewport上に範囲を設け、その中に入っている場合と入っていない場合でIndicator表示を切り替える処理を実装。

--- Assets/Sunchoi/Scripts/Game/Controller/UI/UITargetIndicatorController.cs
+++ Assets/Sunchoi/Scripts/Game/Controller/UI/UITargetIndicatorController.cs
@@ -95,15 +95,39 @@ public class UITargetIndicatorController : MonoBehaviour
             // 比較ターゲットのPosition
             Vector2 toPos = this.toTargetTransform.position;
             Vector2 fromPos = this.fromTargetTransform.position;
-            
-            // 両ターゲットの角度
-            Vector2 diference = toPos - fromPos;
-            float sign = (toPos.y < fromPos.y) ? -1.0f : 1.0f;
-            float angle = Vector2.Angle(Vector2.right, diference) * sign;
 
-            // Indicatorに角度を適用
-            this.indicatorRectTransform.localEulerAngles = new Vector3(0f, 0f, angle);
-        
+            // Indicatorの角度を更新
+            {
+                // 両ターゲットの座標の差分
+                Vector2 diference = toPos - fromPos;
+                // 角度の向き（正か反か）を指定
+                float sign = (toPos.y < fromPos.y) ? -1.0f : 1.0f;
+                // 角度
+                float angle = Vector2.Angle(Vector2.right, diference) * sign;
+
+                // Indicatorに角度を適用
+                this.indicatorRectTransform.localEulerAngles = new Vector3(0f, 0f, angle);
+            }
+
+            // Indicatorの表示是非を更新
+            {
+                // MainCamera
+                var playerCamera = fromTargetTransform.GetComponent<PlayerMovementController>().MainCamera;
+                // Indicatorが表示される範囲の４点を指定
+                Rect rect = new Rect(0f, 0.3f, 1f, 1f);
+                
+                // Viewportで見たターゲットの座標
+                Vector2 targetPosViewportPoint = playerCamera.WorldToViewportPoint(toPos);
+                // ターゲットの座標がRectの範囲に収まっているかどうか
+                bool isOffScreen = rect.Contains(targetPosViewportPoint);
+
+                // 表示切り替え
+                if (isOffScreen)
+                    this.indicatorRectTransform.gameObject.SetActive(false);
+                else
+                    this.indicatorRectTransform.gameObject.SetActive(true);
+            }
+             
             yield return null;
         }
     }
