77991667e5f63e5b39ead0c9c1a55d162e886999
ChoiJihun <choijihun@sunchoi.co.jp>
2022-06-13 12:31:58 +0900

    Playerの各種Status更新処理およびTEXT更新処理を追加。 不要ソースコードを排除。

--- Assets/Sunchoi/Scripts/Game/Manager/Player/PlayerStatusManager.cs
+++ Assets/Sunchoi/Scripts/Game/Manager/Player/PlayerStatusManager.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatusManager : MonoBehaviour
 {
-    #region [01. コンストラクタ]
+    #region [01. Instance]
 
     #region [var]
     /// <summary>
@@ -24,74 +26,256 @@ public class PlayerStatusManager : MonoBehaviour
         // 破棄不可
         DontDestroyOnLoad(this.gameObject);
     }
-
     #endregion
     
     #endregion
     
-    #region [02. Contacing Sources]
+    
+    
+    #region [02. Set Status]
 
     #region [var]
     /// <summary>
-    /// トリガー：Item
+    /// Level
     /// </summary>
-    [Header(" --- Item関連")]
     [SerializeField]
-    private bool isSourceItem = false;
-    public bool IsSourceItem { get => this.isSourceItem; }
+    private int currentLevel;
+    [SerializeField]
+    private int maxLevel = 10;
     /// <summary>
-    /// 現在接触しているItem
+    /// EXP
     /// </summary>
     [SerializeField]
-    private Item currentContactingItem;
+    private int currentExp;
     [SerializeField]
-    private Transform currentContactingColliderTransform;
-    public Item CurrentContactingItem { get => this.currentContactingItem; }
+    private int maxExp;
+    private List<int> maxExpList = new List<int>(){50, 100, 200, 400, 750, 1500, 2300, 3200, 4400, 10000};
     /// <summary>
-    /// UIDialogController
+    /// HP
     /// </summary>
     [SerializeField]
-    private UIDialogController uIDialogController;
+    private int currentHp;
+    [SerializeField]
+    private int maxHP;
     /// <summary>
-    /// UIButtonController
+    /// Attack Damage
     /// </summary>
     [SerializeField]
-    private UIButtonController uIButtonController;
-
+    private int attack;
+    /// <summary>
+    /// Critical Chance
+    /// </summary>
+    [SerializeField]
+    private int critical;
+    /// <summary>
+    /// Defence
+    /// </summary>
+    [SerializeField]
+    private int defence;
+    /// <summary>
+    /// Agility
+    /// </summary>
+    [SerializeField]
+    private int agility;
+    /// <summary>
+    /// Inventory Count
+    /// </summary>
+    [SerializeField]
+    private int currentInventoryCount;
+    [SerializeField]
+    private int maxInventoryCount;
+    /// <summary>
+    /// DoorKey Count
+    /// </summary>
+    [SerializeField]
+    private int currentDoorKeyCount;
+    [SerializeField]
+    private int maxDoorKeyCount = 1;
     #endregion
 
     #region [func]
+    
+    #region [00. Set Status]
+
     /// <summary>
-    /// 現在接触しているItemをセット
+    /// PlayerStatusをセット
     /// </summary>
-    /// <param name="item"></param>
-    public void SetCurrentContactingItem(Item item, Transform collider, bool state)
+    public void SetPlayerStatus()
     {
-        // データセット
-        this.currentContactingItem = item;
-        this.currentContactingColliderTransform = collider;
-        
-        // トリガーセット
-        this.isSourceItem = state;
-        // Interactボタン表示ステートの切り替え
-        this.uIButtonController.SetInteractButtonState(state);
+        // PlayerStatusを初期値に更新
+        // TODO:: 
+        this.InitPlayerStatus();
     }
     
     /// <summary>
-    /// Interactボタン押下時の処理
+    /// PlayerStatusを初期値に更新
     /// </summary>
-    public void OnClickInteractButton()
+    private void InitPlayerStatus()
     {
-        if(this.isSourceItem)
-        {
-            // データセット
-            this.uIDialogController.SetItemDialog(this.currentContactingItem, this.currentContactingColliderTransform);
-            // ItemDialog表示
-            this.uIDialogController.ShowDialog(this.uIDialogController.Dialog_Item.transform, 1);
-        }
+        // 各種Status更新
+        this.SetLevel(1);
+        this.SetExp(0);
+        this.SetHp(100);
+        this.SetMaxHp(100);
+        this.SetAttack(10);
+        this.SetCritical(10);
+        this.SetDefence(5);
+        this.SetAgility(20);
+        this.SetCurrentInventoryCount(0);
+        this.SetMaxInventoryCount(10);
+        this.SetCurrentDoorKeyCount(0);
+        
+        // 各種StatusのTEXTを一斉更新
+        this.SetAllStatusTexts();
+    }
+
+    /// <summary>
+    /// 各種Statusを更新
+    /// </summary>
+    public void SetLevel(int value) { this.currentLevel = value; }
+    public void SetExp(int value) { this.currentExp = value; this.maxExp = this.maxExpList[this.currentLevel - 1]; }
+    public void SetHp(int value) { this.currentHp = value; }
+    public void SetMaxHp(int value) { this.maxHP = value; }
+    public void SetAttack(int value) { this.attack = value; }
+    public void SetCritical(int value) { this.critical = value; }
+    public void SetDefence(int value) { this.defence = value; }
+    public void SetAgility(int value) { this.agility = value; }
+    public void SetCurrentInventoryCount(int value) { this.currentInventoryCount = value; }
+    public void SetMaxInventoryCount(int value) { this.maxInventoryCount = value; }
+    public void SetCurrentDoorKeyCount(int value) { this.currentDoorKeyCount = value; }
+    #endregion
+
+    #endregion
+
+    #endregion
+
+    
+    
+    #region [03. 各種StatusのText更新 関連]
+
+    #region [var]
+    [Header(" --- Level TEXT")]
+    /// <summary>
+    /// Level TEXT
+    /// </summary>
+    [SerializeField]
+    private Text currentLevelText;
+    
+    [Header(" --- EXP TEXT")]
+    /// <summary>
+    /// EXP TEXT
+    /// </summary>
+    [SerializeField]
+    private Text expText;
+    
+    [Header(" --- HP TEXT")]
+    /// <summary>
+    /// HP TEXT
+    /// </summary>
+    [SerializeField]
+    private Text hpText;
+    
+    [Header(" --- Attack TEXT")]
+    /// <summary>
+    /// Attack TEXT
+    /// </summary>
+    [SerializeField]
+    private Text attackText;
+    
+    [Header(" --- Critical TEXT")]
+    /// <summary>
+    /// Critical TEXT
+    /// </summary>
+    [SerializeField]
+    private Text critiaclText;
+    
+    [Header(" --- Defence TEXT")]
+    /// <summary>
+    /// Defence TEXT
+    /// </summary>
+    [SerializeField]
+    private Text defenceText;
+    
+    [Header(" --- Agility TEXT")]
+    /// <summary>
+    /// Agility TEXT
+    /// </summary>
+    [SerializeField]
+    private Text agilityText;
+    
+    [Header(" --- InventoryCount TEXT")]
+    /// <summary>
+    /// InventoryCount TEXT
+    /// </summary>
+    [SerializeField]
+    private Text inventoryCountText;
+    
+    [Header(" --- DoorKeyCount TEXT")]
+    /// <summary>
+    /// DoorKeyCount TEXT
+    /// </summary>
+    [SerializeField]
+    private Text doorKeyCountText;
+    #endregion
+
+
+
+    #region [func]
+    /// <summary>
+    /// 各種StatusのTextを一斉更新
+    /// </summary>
+    private void SetAllStatusTexts()
+    {
+        this.currentLevelText.text = this.currentLevel >= this.maxLevel ? "MAX" : this.currentLevel.ToString();
+        this.expText.text = this.currentExp.ToString() + " / " + this.maxExp.ToString();
+        this.hpText.text = this.currentHp.ToString() + " / " + this.maxHP.ToString();
+        this.critiaclText.text = this.critical.ToString()+ " % ";
+        this.defenceText.text = this.defence.ToString();
+        this.agilityText.text = this.agility.ToString()+ " % ";
+        this.inventoryCountText.text = this.currentInventoryCount.ToString() + " / " + this.maxInventoryCount.ToString();
+        this.doorKeyCountText.text = this.currentDoorKeyCount.ToString() + " / " + this.maxDoorKeyCount.ToString();
+    }
+    
+    /// <summary>
+    /// 各種StatusのTextを更新
+    /// </summary>
+    private void SetLevelText()
+    {
+        this.currentLevelText.text = this.currentLevel >= this.maxLevel ? "MAX" : this.currentLevel.ToString();
+    }
+    private void SetExpText()
+    {
+        this.expText.text = this.currentExp.ToString() + " / " + this.maxExp.ToString();
+    }
+    private void SetHpText()
+    {
+        this.hpText.text = this.currentHp.ToString() + " / " + this.maxHP.ToString();
+    }
+    private void SetAttackText()
+    {
+        this.attackText.text = this.attack.ToString();
+    }
+    private void SetCriticalText()
+    {
+        this.critiaclText.text = this.critical.ToString()+ " % ";
+    }
+    private void SetDefenceText()
+    {
+        this.defenceText.text = this.defence.ToString();
+    }
+    private void SetAgilityText()
+    {
+        this.agilityText.text = this.agility.ToString()+ " % ";
+    }
+    private void SetInventoryCountText()
+    {
+        this.inventoryCountText.text = this.currentInventoryCount.ToString() + " / " + this.maxInventoryCount.ToString();
+    }
+    private void SetDoorKeyCountText()
+    {
+        this.doorKeyCountText.text = this.currentDoorKeyCount.ToString() + " / " + this.maxDoorKeyCount.ToString();
     }
     #endregion
 
     #endregion
-
 }
