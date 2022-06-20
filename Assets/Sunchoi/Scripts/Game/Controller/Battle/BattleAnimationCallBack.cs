21abc9a11b68a662b018eb57046608139297ab40
ChoiJihun <choijihun@sunchoi.co.jp>
2022-06-20 10:42:01 +0900

    Animation再生中の任意のタイミングでEventを発生させるためのBattleAnimationCallBackスクリプトをBattle対象のPlayerおよびEnemyAnimatorにアタッチ。 両UnitのBattleAction（NormalAttack、CriticalAttack）にEvent発生トリガーを設定。

---
+++ Assets/Sunchoi/Scripts/Game/Controller/Battle/BattleAnimationCallBack.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleAnimationCallBack : MonoBehaviour
+{
+    private Action callEventOnPlaying;
+    private Action onCompleted;
+    
+    /// <summary>
+    /// Animation再生中のEventトリガー
+    /// </summary>
+    public void EventOnPlayingAnimationCallBack()
+    {
+        this.callEventOnPlaying?.Invoke();
+    }
+    
+    public void EventOnPlayingAnimation(Action onFinished)
+    {
+        this.callEventOnPlaying = onFinished;
+    }
+    
+    
+    /// <summary>
+    /// Animation再生終了トリガー
+    /// </summary>
+    public void EndAnimationCallBack()
+    {
+        this.onCompleted?.Invoke();
+    }
+    
+    /// <summary>
+    /// Animation再生終了コールバック
+    /// </summary>
+    /// <param name="onCompleted"></param>
+    public void EndEffect(Action onFinished)
+    {
+        this.onCompleted = onFinished;
+    }
+}
